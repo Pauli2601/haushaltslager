d98d92a2dc59b8d00b8b4819ee5794027890734c
Pauli2601 <leopaul2601@gmail.com>
2026-01-02 16:13:10 +0100

    Add Spring Boot skeleton

---
+++ src/main/java/de/pauli2601/haushaltslager/HaushaltslagerApplication.java
@@ -0,0 +1,13 @@
+package de.pauli2601.haushaltslager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaushaltslagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaushaltslagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/pauli2601/haushaltslager/HaushaltslagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pauli2601.haushaltslager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaushaltslagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
